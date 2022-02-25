832e4ac70d648d6de6a06551e97183fc29673a69
Nicolas Mazitelli <nicolas.mazitelli@mercadolibre.com>
2022-02-25 13:12:50 -0300

    add cursor

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -3,136 +3,6 @@ import { useState, createRef, useEffect } from 'react';
 import { css } from '@emotion/react';
 import media from '../../styles/media';
 import NewsIcon from '../icons/news';
-const Styles = css`
-  z-index: 1;
-  position: relative;
-  width: 100vw;
-  overflow: hidden;
-
-  .container {
-    flex-direction: column;
-    align-items: flex-start;
-  }
-  p {
-    margin-top: 20px;
-  }
-  h2 {
-    color: #fff;
-    font-weight: 300;
-    text-align: left;
-    padding-bottom: 40px;
-  }
-  .tag {
-    background: #000;
-    color: #fff;
-    border-radius: 60px;
-    padding: 5px 20px;
-    font-size: 2rem;
-    font-weight: 400;
-    margin-bottom: 60px;
-  }
-  .slider-container {
-    width: 100vw;
-    overflow:scroll;
-    padding-left: calc((100vw - 1200px) / 2 );
-    /* Hide scrollbar for Chrome, Safari and Opera */
-    ::-webkit-scrollbar {
-      display: none;
-    }
-
-    /* Hide scrollbar for IE, Edge and Firefox */
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
-    cursor: grab;
-  }
-
-  .news-container {
-    max-width: 1000vw;
-    width: auto;
-    z-index:3;
-    padding-top: 40px;
-    float: left;
-    display: flex;
-    .new-container {
-      width: 400px;
-      position:relative;
-      background: #14302B;
-      border-radius: 15px;
-      margin-right: 20px;
-      padding:25px;
-
-      .date {
-        color: #B0EFA8;
-      }
-
-      p {
-        color:white;
-        font-size: 1.3rem;
-        line-height: 2rem;
-        font-weight:100;
-      }
-
-      h3 {
-        font-size: 1.6rem;
-        font-weight: 300;
-        padding-top:0;
-        color: #fff;
-      }
-      .link-icon a  {
-        display: flex;
-        flex-direction: row;
-        position: absolute;
-        bottom: 20px;
-        left: 20px;
-        justify-content:center;
-        align-items: center;
-        svg {
-          width: 30px;
-          height: 30px;
-        }
-        span {
-          color: #C8C8C8;
-          font-weight: 100;
-          margin-left: 10px;
-        }
-      }
-      span {
-        font-size: 1.5rem;
-        font-weight: 100;
-        color: #34C55D;
-        width: 100%;
-        display: block;
-      }
-      img {
-        margin-top: 30px;
-        border-radius: 0 0 30px 30px;
-        float:right;
-        margin-bottom:40px;
-      }
-
-    }
-  }
-
-  ${media.medium} {
-    height: 300px;
-    .background {
-      width: 80%;
-      height: 200px;
-      margin: auto;
-    }
-
-    .title {
-      width: 100%;
-      overflow: hidden;
-    }
-
-    h1 {
-      font-size: 3rem;
-      width: 100%;
-      text-align: center;
-    }
-  }
-`;
 
 const news = [
   {
@@ -218,6 +88,137 @@ const News = () => {
     x: 0,
     y: 0,
   });
+
+  const Styles = css`
+    z-index: 1;
+    position: relative;
+    width: 100vw;
+    overflow: hidden;
+
+    .container {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+    p {
+      margin-top: 20px;
+    }
+    h2 {
+      color: #fff;
+      font-weight: 300;
+      text-align: left;
+      padding-bottom: 40px;
+    }
+    .tag {
+      background: #000;
+      color: #fff;
+      border-radius: 60px;
+      padding: 5px 20px;
+      font-size: 2rem;
+      font-weight: 400;
+      margin-bottom: 60px;
+    }
+    .slider-container {
+      width: 100vw;
+      overflow:scroll;
+      padding-left: calc((100vw - 1200px) / 2 );
+      /* Hide scrollbar for Chrome, Safari and Opera */
+      ::-webkit-scrollbar {
+        display: none;
+      }
+
+      /* Hide scrollbar for IE, Edge and Firefox */
+      -ms-overflow-style: none;  /* IE and Edge */
+      scrollbar-width: none;  /* Firefox */
+      cursor: ${ isDragging ? 'grab' : 'pointer' };
+    }
+
+    .news-container {
+      max-width: 1000vw;
+      width: auto;
+      z-index:3;
+      padding-top: 40px;
+      float: left;
+      display: flex;
+      .new-container {
+        width: 400px;
+        position:relative;
+        background: #14302B;
+        border-radius: 15px;
+        margin-right: 20px;
+        padding:25px;
+
+        .date {
+          color: #B0EFA8;
+        }
+
+        p {
+          color:white;
+          font-size: 1.3rem;
+          line-height: 2rem;
+          font-weight:100;
+        }
+
+        h3 {
+          font-size: 1.6rem;
+          font-weight: 300;
+          padding-top:0;
+          color: #fff;
+        }
+        .link-icon a  {
+          display: flex;
+          flex-direction: row;
+          position: absolute;
+          bottom: 20px;
+          left: 20px;
+          justify-content:center;
+          align-items: center;
+          svg {
+            width: 30px;
+            height: 30px;
+          }
+          span {
+            color: #C8C8C8;
+            font-weight: 100;
+            margin-left: 10px;
+          }
+        }
+        span {
+          font-size: 1.5rem;
+          font-weight: 100;
+          color: #34C55D;
+          width: 100%;
+          display: block;
+        }
+        img {
+          margin-top: 30px;
+          border-radius: 0 0 30px 30px;
+          float:right;
+          margin-bottom:40px;
+        }
+
+      }
+    }
+
+    ${media.medium} {
+      height: 300px;
+      .background {
+        width: 80%;
+        height: 200px;
+        margin: auto;
+      }
+
+      .title {
+        width: 100%;
+        overflow: hidden;
+      }
+
+      h1 {
+        font-size: 3rem;
+        width: 100%;
+        text-align: center;
+      }
+    }
+  `;
   
   const myRef = createRef();
 
